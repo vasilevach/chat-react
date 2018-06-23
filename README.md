cb5ddb60fac101870fa9353937e7389200f15c40
Chris Panayotova <vasileva.ch@gmail.com>
2018-06-23 10:07:40 +0300

    Start developing the chat app on a small boilrplate extended from minimal-react-webpack-babel

---
+++ README.md
@@ -0,0 +1,1 @@
+So I am starting this exercise. I build a little boilerplate on the top of minimal-react-webpack-babel.
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message) => {
+
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  //send immediatly a feedback to the incoming connection
+  ws.send('Hi there, I am a WebSocket server');
+});
+
+server.listen(process.env.PORT || 8999, () => {
+  console.log(`Server started on port ${server.address().port} :)`);
+});
